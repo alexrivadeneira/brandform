7d3c45a5b3ba32ce55f36c367b68c4ac7812f82e
alex <alexrivs@gmail.com>
2016-09-22 11:37:19 -0700

    start

---
+++ app.rb
@@ -0,0 +1,41 @@
+# app.rb
+require 'sinatra'
+# require 'Haml'
+require 'sinatra/activerecord'
+require './config/environments' #database configuration
+require 'pry'
+require './models/model' 
+
+get '/' do
+	@model = Model.new(name: "Hi")
+	@model.save
+	@models = Model.all
+  	erb :test
+end
+
+get '/contact' do
+	haml :contact
+end	
+
+get '/form' do
+	haml :form
+end	
+
+post '/submit' do
+
+	puts params["community"]
+
+	@model = Model.new(name: params)
+	@model.save
+
+	redirect '/'
+	# if @model.save
+	# 	redirect '/'
+	# else
+	# 	"Sorry, there was an error!"
+	# end
+end
+
+
+
+# binding.pry
